671c5cbcfae8cf5c84c5fdfae7b0e22d77e9e979
zkllll23 <133302305@qq.com>
2023-07-15 18:19:07 +0800

    build: 初始化项目并构建依赖

---
+++ my-leadnews-common/src/main/java/io/github/zkllll23/common/exception/CustomException.java
@@ -0,0 +1,16 @@
+package io.github.zkllll23.common.exception;
+
+import io.github.zkllll23.model.common.enums.AppHttpCodeEnum;
+
+public class CustomException extends RuntimeException {
+
+    private AppHttpCodeEnum appHttpCodeEnum;
+
+    public CustomException(AppHttpCodeEnum appHttpCodeEnum){
+        this.appHttpCodeEnum = appHttpCodeEnum;
+    }
+
+    public AppHttpCodeEnum getAppHttpCodeEnum() {
+        return appHttpCodeEnum;
+    }
+}
